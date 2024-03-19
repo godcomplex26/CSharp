1b28679575dd15f34f41b840f1ef3a82da2aadd4
jongwoonL <whddns46@naver.com>
2024-03-19 10:50:51 +0900

    update HelloCSharp001_3/Program.cs

--- 01_start_c#/ConsoleApp1/HelloCSharp001_3/Program.cs
+++ 01_start_c#/ConsoleApp1/HelloCSharp001_3/Program.cs
@@ -14,7 +14,9 @@ namespace HelloCSharp001_3
             Console.WriteLine("인치를 입력해 보세요.");
             double inch = double.Parse(Console.ReadLine());
             double cm = inch * 2.54;
-            Console.WriteLine(inch + "인치는 " + cm + "cm 입니다.");
+            Console.WriteLine(inch + "inch는 " + cm + "cm 입니다.");
+            Console.WriteLine($"{inch}inch는 {cm}cm 입니다.");
+            Console.WriteLine(String.Format("{0}inch는 {1}cm 입니다.", inch, cm));
             Console.WriteLine();
 
             // kg -> 파운드
@@ -32,6 +34,11 @@ namespace HelloCSharp001_3
             double area = pi * rad * rad;
             Console.WriteLine("반지름이 " + rad + "인 원 둘레는 " + cir + "입니다.");
             Console.WriteLine("반지름이 " + rad + "인 원 넓이는 " + area + "입니다.");
+
+            // 참고 주의사항(괄호로 잘 묶기)
+            Console.WriteLine(10 + 2 + "100"); // 12100 : 숫자 연산, 문자열 병합
+            Console.WriteLine(10 + "100" + 2); // 101002 : 문자열 병합, 문자열 병합
+            Console.WriteLine(10 + 2 + "100" + 10 + 2); // 12100102 : 숫자 연산, 문자열 병합, 문자열 병합, 문자열 병합
         }
     }
 }
