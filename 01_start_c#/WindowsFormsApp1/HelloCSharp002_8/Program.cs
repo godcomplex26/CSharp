d446db4a2ab9e9e14a758f6330012d971883ff38
jongwoonL <whddns46@naver.com>
2024-03-20 16:08:43 +0900

    오버로딩

---
+++ 01_start_c#/WindowsFormsApp1/HelloCSharp002_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp002_8
+{
+    internal class Program
+    {
+        // 오버로딩 : 매개변수의 개수나 타입 다르면
+        // 함수(=메서드) 이름 똑같아도 서로 다른 것으로
+        // java 와 동일
+        static void introduce(int age)
+        {
+            Console.WriteLine("내 나이는 " + age + "입니다.");
+        }
+
+        static void introduce(string name)
+        {
+            Console.WriteLine("내 이름은 " + name + "입니다.");
+        }
+
+        static void introduce()
+        {
+            Console.WriteLine("자기 소개 메서드");
+        }
+
+        static void Main(string[] args)
+        {
+            introduce(30);
+            introduce("이종운");
+            introduce();
+        }
+    }
+}
---
+++ 01_start_c#/WindowsFormsApp1/HelloCSharp002_8/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp002_8
+{
+    internal class Rectangle // 사각형
+    {
+        public int width {  get; set; } // 각 사각형마다
+        public int height { get; set; } // 너비, 높이 다름
+        public static string color { get; set; } // 모두 색깔은 같다고 가정
+
+        // 인스턴스 메서드(인스턴스에 따라 결과 변함)
+        public int getArea() { return width * height; } // 자신의 넓이 계산
+
+        // 클래스 메서드(매개변수에 따라 결과 변함)
+        public static int calcRecArea(int w, int h) { return w * h; } // 해당 매개 변수 반영하여 넓이 계산
+        
+        // 클래스 메서드를 인스턴스를 활용하여 사용
+        public static int calcRecArea(Rectangle rect) { return rect.width * rect.height;} 
+    }
+}
