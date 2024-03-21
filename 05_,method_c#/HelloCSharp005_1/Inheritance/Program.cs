6c85ad01882f9b8ae5beab16f41b13b9455c85f6
jongwoonL <whddns46@naver.com>
2024-03-21 15:15:34 +0900

    다형성

--- 05_,method_c#/HelloCSharp005_1/Inheritance/Program.cs
+++ 05_,method_c#/HelloCSharp005_1/Inheritance/Program.cs
@@ -47,6 +47,58 @@ namespace Inheritance
             zoo.Add(b);
             zoo.Add(c);
             zoo.Add(d);
+            zoo.Add(new Animal());
+            zoo.Add(new Dog());
+            zoo.Add(new Cat());
+
+            // 다형성이란?
+            // 다양한 형태로 변할 가능성
+            // A is B, 스마트폰 is 전화기 O
+            // 전화기 is 스마트폰 X
+
+            // Cat is Animal O
+            // Animal is Cat X
+            // 조상 temp = new 자손(); O // 업캐스팅
+            Animal aaa = new Cat();
+            Animal bbb = new Dog();
+            Object o = new Animal(); // 모든 클래스의 조상 Object, object(소문자도 됨)
+
+            // aaa.meow() 하려면 다운캐스팅 필요(업된거만 다운가능)
+            ((Cat)(aaa)).Meow();
+
+            if(aaa is Cat) // java에서는 instancOf
+            {
+                ((Cat)(aaa)).Meow();
+                (aaa as Cat).Meow();
+            }
+
+            var tempCat = aaa as Cat;
+            if (tempCat != null) 
+            {
+                tempCat.Meow();
+            }
+            if (tempCat == null)
+            {
+                Console.WriteLine("고양이가 아님");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var item in zoo)
+            {
+                item.Sleep(); // 공통 기능
+                item.LivingWorld(); // 공통 기능
+
+                if (item is Cat)
+                {
+                    (item as Cat).Meow();
+                }
+
+                if (item is Dog)
+                {
+                    (item as Dog).Bark();
+                }    
+            }
         }
     }
 }
