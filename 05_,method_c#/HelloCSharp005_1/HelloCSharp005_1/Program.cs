bd35a18dac39be8a16068b38d0f3cb3a6037d559
jongwoonL <whddns46@naver.com>
2024-03-21 09:46:00 +0900

    복사 추가 설명

--- 05_,method_c#/HelloCSharp005_1/HelloCSharp005_1/Program.cs
+++ 05_,method_c#/HelloCSharp005_1/HelloCSharp005_1/Program.cs
@@ -13,6 +13,23 @@ namespace HelloCSharp005_1
         {
             input++;
         }
+
+        // 클래스 말고, 배열이나 List도 똑같이 참조 복사 개념 동일함
+        // 구조체 Struct는 일반 자료형 처럼 값 복사 개념 적용
+        static void change(Student student)
+        {
+            student.name = "김길동";
+            student.age = 20;
+            student.score = 90;
+        }
+
+        // ref = 참조자
+        // 메서드 안에서 값이 바뀌면 원본도 같이 바뀜
+        static void change(ref int input)
+        {
+            input++;
+        }
+
         static void Main(string[] args)
         {
             int a = 10;
@@ -20,7 +37,9 @@ namespace HelloCSharp005_1
             b = 20;
             Console.WriteLine("a=" + a + ", b=" + b); // 서로 영향 x -> 파일을 복사 붙여넣기 한 것
             change(a);
-            Console.WriteLine("a=" + a);
+            Console.WriteLine("a=" + a); // 10
+            change(ref a);
+            Console.WriteLine("a=" + a); // 11
 
             Student s = new Student();
             s.name = "이종운";
@@ -36,6 +55,19 @@ namespace HelloCSharp005_1
 
             Console.WriteLine(s.name + ", " + s.age + ", " + s.score);
             Console.WriteLine(dj.name + ", " + dj.age + ", " + dj.score);
+
+            Student s2 = new Student(); // 제대로 복사 된 것
+            s2.name = "홍길동";
+            s2.age = s.age;
+            s2.score = 80;
+
+            Console.WriteLine(s2.age);
+            s2.age = 40; // 값 복사라서 영향 x
+            Console.WriteLine(s.age); // 30
+            Console.WriteLine(s2.age); // 40
+
+            change(s2); // 김길동으로 바뀐다
+            Console.WriteLine(s2.name + ", " + s2.age + ", " + s2.score);
         }
     }
 }
