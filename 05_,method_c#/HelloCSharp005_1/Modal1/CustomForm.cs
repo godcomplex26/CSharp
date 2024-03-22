25e4d3832f27e36db3a5441d6e14d65c736a1587
jongwoonL <whddns46@naver.com>
2024-03-22 12:40:06 +0900

    모달 시작

---
+++ 05_,method_c#/HelloCSharp005_1/Modal1/CustomForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // Form 상속 받기 위해 필요
+
+namespace Modal1
+{
+    public class CustomForm : Form
+    {
+        public CustomForm()
+        {
+
+        }
+    }
+}
