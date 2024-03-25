b6e80bfb5ca3209b5fb863ebf322115552f4f2d7
jongwoonL <whddns46@naver.com>
2024-03-25 11:42:45 +0900

    제네릭과 TryParse

---
+++ 05_,method_c#/HelloCSharp005_1/HelloCSharp005_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp005_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 제네릭 활용 예시
+            Student<int> s1 = new Student<int>();
+            Student<String> s2 = new Student<String>();
+
+            s1.hakbeon = int.Parse("0123");
+            s2.hakbeon = "0123";
+
+            Console.WriteLine("s1의 학번 : " +  s1.hakbeon); // 123
+            Console.WriteLine("s2의 학번 : " +  s2.hakbeon); // 0123
+
+            /*            int a = int.Parse("aaa");
+                        Console.WriteLine("프로그램이 튕기므로 이 부분 실행 안 됨");
+            */
+
+            // TryParse는 Parse를 시도해서 되면 하고
+            // 안 되면 false값을 반환
+            int a;
+            int.TryParse("aaa", out a);
+            int.TryParse("aaa", out int b);
+            bool fa = int.TryParse("aaa", out a);
+            bool fb = int.TryParse("aaa", out b);
+            Console.WriteLine("a = " + a + ", b = " + b);
+            Console.WriteLine("fa = " + fa + ", fb = " + fb);
+        }
+    }
+}
---
+++ 05_,method_c#/HelloCSharp005_1/HelloCSharp005_6/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp005_6
+{
+    // 제네릭
+    // T 대신 다른 문자 넣어도 됨
+    // 특정 속성의 타입을 정하지 못한 경우
+    // 임의의 문자로 타입을 지정하고
+    // 인스턴스 생성 시 그 때 타입을 정하는 방식
+    // 가장 대표적인 예시 List
+    // ex. Student<int> s 하면 학번은 정수가 됨
+    // Student<String> s 하면 학번은 문자열이 됨
+    public class Student<T>
+    {
+        public T hakbeon { get; set; }
+        public string name { get; set; }
+    }
+}
