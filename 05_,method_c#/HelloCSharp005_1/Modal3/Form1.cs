ebe2edeedad5d8dbaf5b9ef43ebb6ae743c9cbd0
jongwoonL <whddns46@naver.com>
2024-03-25 11:23:31 +0900

    모달 연습

---
+++ 05_,method_c#/HelloCSharp005_1/Modal3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modal3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            string text = textBox1.Text.ToLower();
+            new Form2(text).ShowDialog(); // 모달
+            textBox1.Text = "";
+            Show();
+        }
+    }
+}
---
+++ 05_,method_c#/HelloCSharp005_1/Modal3/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modal3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(String text)
+        {
+            InitializeComponent();
+            if(text.Equals("red"))
+            {
+                this.BackColor = Color.Red;
+            }
+            
+            if(text.Equals("blue"))
+            {
+                this.BackColor = Color.Blue;
+            }
+        }
+    }
+}
