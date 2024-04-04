25fed61b4166e93718f38e3d16378543543072a4
jongwoonL <whddns46@naver.com>
2024-04-04 11:29:55 +0900

    스크롤바 조정1

--- teamProject/teamProject/teamProject/Form1.Designer.cs
+++ teamProject/teamProject/teamProject/Form1.Designer.cs
@@ -158,7 +158,7 @@
             this.ToolStrip4});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1171, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(1195, 25);
             this.menuStrip1.TabIndex = 18;
             this.menuStrip1.Text = "데이터 관리";
             // 
@@ -203,7 +203,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1171, 1108);
+            this.ClientSize = new System.Drawing.Size(1195, 1108);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
